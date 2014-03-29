096d151fb7f18b7134bddd8e4ac80568cac33aa5
Naxus28 <gabrielferraz@Gabriel-Ferrazs-MacBook-Pro.local>
2014-03-29 18:13:43 -0400

    fixed the modal on chrome - needs some alignment

---
+++ ferraz_gabriel_delivery/js/initial_accordion.js
@@ -0,0 +1,23 @@
+// $(window).load(function(){
+// 	$('#jobs').accordion();
+// 	$("#jobs h3").css("margin-bottom","20px");
+// });
+
+// var myRequest = new XMLHttpRequest();
+// 
+// myRequest.onreadystatechange = function(){
+// 	console.log("i exist");
+// 	console.log(myRequest.readyState);
+// 	if(myRequest.readyState===4){
+// 		$('#jobs').click(function(){
+// 			$('#jobs').css("display","none");
+// 			this.#image_div=("<div></div>");
+// 			this.#image=("<img></img>").
+// 			('#image_div').append()
+// 		});
+// 	}
+// };
+// 
+// myRequest.open("GET","images/circle.png", true);
+// 
+// myRequest.send(null);
